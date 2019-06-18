561fded26a0dd30eba904f2537595d7f3542c26b
KOUSUKE0 <zeal17310015@c-league.jp>
2019-06-18 14:32:52 +0900

    管理者一覧画面の作成

---
+++ lampTeamH/lampTeamH/Smarty/templates_c/2b7a163f7097f740f0cb4b1204ae6b57f21bf332_0.file.gmenu.tmpl.php
@@ -0,0 +1,38 @@
+<?php
+/* Smarty version 3.1.30, created on 2019-06-04 13:57:49
+  from "/home/e09/public_html/PHPBase-master/Smarty/templates/gmenu.tmpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5cf5fa4d8cf643_98592817',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '2b7a163f7097f740f0cb4b1204ae6b57f21bf332' => 
+    array (
+      0 => '/home/e09/public_html/PHPBase-master/Smarty/templates/gmenu.tmpl',
+      1 => 1559529856,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5cf5fa4d8cf643_98592817 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!-- ぱんクズ　-->
+<div id="pan">
+<table >
+<tr>
+<td><a href="index.php">メインメニュー</a></td>
+<td><a href="prefecture_list.php">都道府県管理</a></td>
+<td><a href="member_list.php">メンバー管理</a></td>
+<td><a href="member_list_smarty.php">メンバー管理(Smarty版)</a></td>
+</tr>
+</table>
+</div>
+<!-- /ぱんクズ　-->
+<?php }
+}
