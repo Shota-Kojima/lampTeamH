5e3fda1d8420b41acd536ddd23cd952fa8a1e6cb
KOUSUKE0 <zeal17310015@c-league.jp>
2019-06-26 13:20:25 +0900

    管理者一覧のヘッダー追加

--- sources/manager_list.php
+++ sources/manager_list.php
@@ -2,5 +2,5 @@
 require_once("inc_base.php");
 require_once($CMS_COMMON_INCLUDE_DIR . "libs.php");
 require_once("inc_smarty.php");
-$smarty->display('manager.tmpl');
+$smarty->display('manager_list.tmpl');
 ?>
