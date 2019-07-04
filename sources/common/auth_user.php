2d2864219a15affbe5c6694a39e010d6d85457a7
ナナシーナ <vitavita3363@yahoo.co.jp>
2019-07-04 14:51:08 +0900

    test

--- sources/common/auth_user.php
+++ sources/common/auth_user.php
@@ -13,32 +13,30 @@ session_start();
 $row = array();
 $customer_obj = new ccustomer();
 $cart_obj = new ccart();
+$product_obj = new cproductH();
 if(isset($_SESSION['HTeam_adm']['customer_id'])&& 
     $_SESSION['HTeam_adm']['customer_id'] !== ''){
     //------------------------
     //ユーザの情報を取得
     //------------------------
-	$productarr = $product_obj->get_tgt(false,$_SESSION['HTeam_adm']['customer_id']);
+	$customerarr = $customer_obj->get_tgt(false,$_SESSION['HTeam_adm']['customer_id']);
 	if($productarr !== false){
         //セッション割り当て
-        $_SESSION['HTeam_adm']['customer_email'] = $productarr['customer_email'];
-        $_SESSION['HTeam_adm']['last_name'] = $productarr['last_name'];
-        $_SESSION['HTeam_adm']['last_name_kana'] = $productarr['last_name_kana'];
-        $_SESSION['HTeam_adm']['first_name'] = $productarr['first_name'];
-        $_SESSION['HTeam_adm']['first_name_kana'] = $productarr['first_name_kana'];
-        $_SESSION['HTeam_adm']['postal_code'] = $productarr['postal_code'];
-        $_SESSION['HTeam_adm']['icon_pass'] = $productarr['icon_pass'];
-        $_SESSION['HTeam_adm']['customer_point'] = $productarr['customer_point'];
-        $_SESSION['HTeam_adm']['postal_code'] = $productarr['postal_code'];
-        $_SESSION['HTeam_adm']['customer_created_date'] = $productarr['customer_created_date'];
-        $_SESSION['HTeam_adm']['customer_sex'] = $productarr['customer_sex'];
-        $_SESSION['HTeam_adm']['postal_code'] = $productarr['postal_code'];	
+        $_SESSION['HTeam_adm']['customer_email'] = $customerarr['customer_email'];
+        $_SESSION['HTeam_adm']['last_name'] = $customerarr['last_name'];
+        $_SESSION['HTeam_adm']['last_name_kana'] = $customerarr['last_name_kana'];
+        $_SESSION['HTeam_adm']['first_name'] = $customerarr['first_name'];
+        $_SESSION['HTeam_adm']['first_name_kana'] = $customerarr['first_name_kana'];
+        $_SESSION['HTeam_adm']['postal_code'] = $customerarr['postal_code'];
+        $_SESSION['HTeam_adm']['icon_pass'] = $customerarr['icon_pass'];
+        $_SESSION['HTeam_adm']['customer_point'] = $customerarr['customer_point'];
+        $_SESSION['HTeam_adm']['postal_code'] = $customerarr['postal_code'];
+        $_SESSION['HTeam_adm']['customer_created_date'] = $customerarr['customer_created_date'];
+        $_SESSION['HTeam_adm']['customer_sex'] = $customerarr['customer_sex'];
+        $_SESSION['HTeam_adm']['postal_code'] = $customerarr['postal_code'];	
 		$smarty->assign('session',$_SESSION);
 	}else{
-        // ステータスコードを出力
-        http_response_code( 301 ) ;
-        // リダイレクト
-        header( "Location: ./loginH.php" ) ;
+        
         exit ;
     }
     //------------------------
@@ -59,12 +57,13 @@ if(isset($_SESSION['HTeam_adm']['customer_id'])&&
             $productarr = $product_obj->get_tgt(false,$product_id);
             //取得出来たら商品の金額をセッションに格納
             if($productarr !== false){
-                $product_sum = $cartarr['product_value'] * $productarr["price"];
+                $product_sum += $cartarr['product_value'] * $productarr["price"];
                 $_SESSION['HTeam_adm']['product_count'] = $productarr['product_count'];
                 $_SESSION['HTeam_adm']['product_sum'] = $productarr['product_sum'];
                 //var_dump($productarr);
                 $smarty->assign('session',$_SESSION);
             }else{
+                echo '<script type="text/javascript">alert("66のelse");</script>';
                  // ステータスコードを出力
                 http_response_code( 301 ) ;
                 // リダイレクト
@@ -73,23 +72,13 @@ if(isset($_SESSION['HTeam_adm']['customer_id'])&&
             }
         }
 	}else{
+        echo '<script type="text/javascript">alert("75のelse");</script>';
         // ステータスコードを出力
-        http_response_code( 301 ) ;
-        // リダイレクト
-        header( "Location: ./loginH.php" ) ;
         exit ;
     }
 
-    $row = $customer_obj->get_tgt_login(false,$_SESSION[$CMS_SESSION_USER_BASE_NAME]['member_login']);
-    if($row === false 
-        || !isset($row['general_user_id'])
-        || $row['general_user_id'] != $_SESSION[$CMS_SESSION_USER_BASE_NAME]['general_user_id']){
-        //ゲスト
-        $_SESSION[$CMS_SESSION_USER_BASE_NAME]['general_user_id'] = 0;
-        $_SESSION[$CMS_SESSION_USER_BASE_NAME]['member_login'] = '';
-        $_SESSION[$CMS_SESSION_USER_BASE_NAME]['general_user_name'] = 'ゲスト';
-    }
 }else{
+    echo '<script type="text/javascript">alert("81のelse");</script>';
     // ステータスコードを出力
 	http_response_code( 301 ) ;
 	// リダイレクト
