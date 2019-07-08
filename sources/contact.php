4b1934f1c3342227901bbd13f05cc9e42706a97e
Shota-Kojima <zeal17310029@c-league.jp>
2019-07-09 03:13:03 +0900

    a

--- sources/contact.php
+++ sources/contact.php
@@ -5,5 +5,35 @@ require_once("inc_smarty.php");
 require_once($CMS_COMMON_INCLUDE_DIR . "auth_user.php");
 $smarty->assign('session',$_SESSION);
 $smarty->assign('cart',$_SESSION);
+
+if(isset($_POST['customer_email']) && isset($_SESSION['HTeam_adm']['customer_id']) && isset($_POST['contact_text'])) {
+    regist();
+	
+	
+}else{
+	
+}
+
+//--------------------------------------------------------------------------------------
+/*!
+@brief	メンバーデータの追加／更新。保存後自分自身を再読み込みする。
+@return	なし
+*/
+//--------------------------------------------------------------------------------------
+function regist(){
+    $date = date('YmdHis');
+    $dataarr = array();
+    $dataarr['customer_id'] = (string)$_SESSION['HTeam_adm']['customer_id'];
+    $dataarr['customer_email'] = (string)$_POST['customer_email'];
+    $dataarr['receive_date'] = $date;
+    $dataarr['contact_text'] = (string)$_POST['contact_text'];
+    $dataarr['reply_flag'] = (int)0;
+    
+    $chenge = new cchange_ex();
+    $mid = $chenge->insert('contact',$dataarr);
+    echo '<script type="text/javascript">alert("送信しました(送信完了画面に遷移予定)");</script>';
+}
+
+
 $smarty->display('contact.tmpl');
 ?>
