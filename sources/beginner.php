0e2add43492df28afc8d2b78e9e080743c1b5ea1
hossyan36 <hmshr36@gmail.com>
2019-07-10 09:39:58 +0900

    ブラックリスト画面追加

--- sources/beginner.php
+++ sources/beginner.php
@@ -5,5 +5,5 @@ require_once("inc_smarty.php");
 require_once($CMS_COMMON_INCLUDE_DIR . "auth_user.php"); 
 $smarty->assign('session',$_SESSION); 
 $smarty->assign('cart',$_SESSION);
-$smarty->display('admini_review.tmpl');
+$smarty->display('beginner.tmpl');
 ?>
