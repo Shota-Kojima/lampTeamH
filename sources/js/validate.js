c378823a4d8799761d00b58a4cb47e027474a86c
ナナシーナ <vitavita3363@yahoo.co.jp>
2019-07-10 10:14:03 +0900

    不要ファイルの削除

--- sources/js/validate.js
+++ sources/js/validate.js
@@ -12,12 +12,30 @@ $('#purchase').validate({
     },
     messages: {
         trigger: {
-            required: "チェックしてない",
-            email: "....?"
+            required: "お支払方法を選択してください"
         },
         survival: {
-            required: "チェックしてない",
-            dateISO: "....?"
+            required: "ポイント利用を選択してください"
+        }
+    },
+    //エラーメッセージの表示場所を設定
+    //表示位置指定
+    errorPlacement: function(error, element) {
+        switch(element.attr('name')) {
+        case "trigger":
+          error.insertAfter($('#trigger_error'));
+          break;
+        case "survival":
+          error.insertAfter($('#survival_error'));
+          break;
+        case "field3":
+          error.insertAfter($('#field3_error'));
+          break;
+        case "field4":
+          error.insertAfter($('#field4_error'));
+          break;
+        default:
+          error.insertAfter(element);
         }
     }
 });
