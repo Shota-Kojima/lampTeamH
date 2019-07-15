6f5369ce9d9d0cd6c5638ac965e997583974d6f9
ナナシーナ <vitavita3363@yahoo.co.jp>
2019-07-15 16:49:08 +0900

    update

--- sources/admini_review.php
+++ sources/admini_review.php
@@ -10,7 +10,6 @@ if(isset($_SESSION['HTeam_adm']['customer_id']) && $_SESSION['HTeam_adm']['custo
     $review_obj = new creview();
     $transInfo_obj = new ctransaction_info();
     $reviewarr = $review_obj->get_allH(false);
-    var_dump($reviewarr);
     $view = array();
 
     for($i = 0; $i < count($reviewarr); $i++){
--- sources/review_details.php
+++ sources/review_details.php
@@ -8,21 +8,18 @@ if(isset($_GET['transaction_id']) && isset($_GET['product_id']) &&
     $review_obj = new creview();
     $reviewarr = $review_obj->get_tgtH(false,$_GET['transaction_id'],$_GET['product_id']);
     // var_dump($reviewarr);
+    $bday = new DateTime($reviewarr['review_date']);
     
     $view = array(
-        'product_name'=> (String)$productarr,
-        'product_name'=> (int)$_GET['product_name'],
+        'product_name'=> $_GET['product_name'],
         'customer_id'=> (String)$_GET['customer_id'],
         'transaction_id'=> (int)$_GET['transaction_id'],
-        'review_tittle'=> (String)$reviewarr[$i]['review_tittle'],
-        'review_value'=> (String)$reviewarr[$i]['review_value'],
-        'review'=> (String)$reviewarr[$i]['review'],
-        'review_date'=> (String)$reviewarr[$i]['review_date'],
+        'review_tittle'=> (String)$reviewarr['review_tittle'],
+        'review_value'=> (String)$reviewarr['review_value'],
+        'review'=> (String)$reviewarr['review'],
+        'review_date'=> $bday->format('Y年m月d日'),
     );
-    var_dump($view);
     $smarty->assign('view',$view);
 }
-
-
 $smarty->display('review_details.tmpl');
 ?>
