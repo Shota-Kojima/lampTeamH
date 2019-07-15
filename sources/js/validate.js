184fe017ff740a08bd462b74613a6cea67c033b5
ナナシーナ <vitavita3363@yahoo.co.jp>
2019-07-15 22:27:09 +0900

    update

--- sources/js/validate.js
+++ sources/js/validate.js
@@ -34,10 +34,65 @@ $('#purchase').validate({
         }
     }
 });
+
 //お問い合わせ画面
 $('#contact').validate({
     rules: {
         customer_email: {
+            required: true,
+            email:true
+        },
+        contact_text: {
+            required: true 
+        }
+    },
+    messages: {
+        customer_email: {
+            required: "メールアドレスが入力されていません",
+            email:"メールアドレスの形式で入力してください"
+        },
+        contact_text: {
+            required: "お問い合わせ内容が入力されていません"
+        }
+    },
+    //エラーメッセージの表示場所を設定
+    //表示位置指定
+    errorPlacement: function(error, element) {
+        switch(element.attr('name')) {
+        case "customer_email":
+          error.insertAfter($('#customer_email_error'));
+          break;
+        case "contact_text":
+          error.insertAfter($('#contact_text_error'));
+          break;
+        default:
+          error.insertAfter(element);
+        }
+    }
+});
+
+$('#register').validate({
+    rules: {
+        sex: {
+            required: true
+        },
+        contact_text: {
+            required: true 
+        },
+        customer_email: {
+            required: true,
+            email:true
+        },
+        contact_text: {
+            required: true 
+        },
+        customer_email: {
+            required: true,
+            email:true
+        },
+        contact_text: {
+            required: true 
+        },customer_email: {
             required: true,
             email:true
         },
