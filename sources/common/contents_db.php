3beaa0616d39b840fb51c8a86bbf498ddd5413fd
Syunsuke Yoshida <yoshida9800@gmail.com>
2019-07-15 15:03:02 +0900

    contents_dbの修正

--- sources/common/contents_db.php
+++ sources/common/contents_db.php
@@ -473,7 +473,62 @@ class cproductH extends crecord {
 	@return	配列（2次元配列になる）
 	*/
 	//--------------------------------------------------------------------------------------
-    public function get_all_order($debug,$from,$limit,$conditions,$sort,$tgt_category){
+    public function get_all_order($debug,$from,$limit,$conditions,$tgt_genre,$tgt_culmn,$tgt_category){
+		$arr = array();
+		//親クラスのselect()メンバ関数を呼ぶ
+		$this->select(
+			$debug,			//デバッグ表示するかどうか
+			"*",			//取得するカラム
+			"productH",	//取得するテーブル
+			"product_category=" .$tgt_category." and "
+			 .$conditions .$tgt_genre,	//条件
+			$tgt_culmn,	//並び替え
+			"limit " . $from . "," . $limit		//抽出開始行と抽出数
+		);
+		//順次取り出す
+		while($row = $this->fetch_assoc()){
+			$arr[] = $row;
+		}
+		//取得した配列を返す
+		return $arr;
+	}
+	public function get_all_genre_count($debug,$conditions,$tgt_genre){
+		$arr = array();
+		//親クラスのselect()メンバ関数を呼ぶ
+		$this->select(
+			$debug,			//デバッグ表示するかどうか
+			"count(*)",			//取得するカラム
+			"productH",	//取得するテーブル
+			$conditions .$tgt_genre			//条件
+		);
+		if($row = $this->fetch_assoc()){
+			//取得した個数を返す
+			return $row['count(*)'];
+		}
+		else{
+			return 0;
+		}
+	}
+	public function get_all_category_genre_count($debug,$conditions,$tgt_genre,$tgt_category){
+		$arr = array();
+		//親クラスのselect()メンバ関数を呼ぶ
+		$this->select(
+			$debug,			//デバッグ表示するかどうか
+			"count(*)",			//取得するカラム
+			"productH",	//取得するテーブル
+			"product_category=" .$tgt_category." and "
+			 .$conditions .$tgt_genre		//条件
+		);
+		if($row = $this->fetch_assoc()){
+			//取得した個数を返す
+			return $row['count(*)'];
+		}
+		else{
+			return 0;
+		}
+	}
+
+	public function get_tgt_order($debug,$from,$limit,$conditions,$sort,$tgt_category){
 		$arr = array();
 		//親クラスのselect()メンバ関数を呼ぶ
 		$this->select(
@@ -492,7 +547,7 @@ class cproductH extends crecord {
 		//取得した配列を返す
 		return $arr;
 	}
-	public function get_all_genre_count($debug,$conditions,$tgt_genre){
+	public function get_tgt_genre_count($debug,$conditions,$tgt_genre){
 		$arr = array();
 		//親クラスのselect()メンバ関数を呼ぶ
 		$this->select(
@@ -509,7 +564,7 @@ class cproductH extends crecord {
 			return 0;
 		}
 	}
-	public function get_all_category_genre_count($debug,$conditions,$tgt_category){
+	public function get_tgt_category_genre_count($debug,$conditions,$tgt_category){
 		$arr = array();
 		//親クラスのselect()メンバ関数を呼ぶ
 		$this->select(
@@ -856,26 +911,6 @@ class ctransaction_info extends crecord {
 	
 	//--------------------------------------------------------------------------------------
 	/*!
-	@brief	指定されたIDの配列を得る
-	@param[in]	$debug	デバッグ出力をするかどうか
-	@param[in]	$id		ID
-	@return	配列（1次元配列になる）空の場合はfalse
-	*/
-	//--------------------------------------------------------------------------------------
-	//上はlikeこっちは＝
-	public function get_tgtH($debug,$id){
-        //親クラスのselect()メンバ関数を呼ぶ
-        $this->select(
-            $debug,         //デバッグ表示するかどうか
-            "*",          //取得するカラム
-            "transaction_info",    //取得するテーブル
-            "transaction_id = '{$id}'"    //条件
-        );
-        return $this->fetch_assoc();
-	}
-
-	//--------------------------------------------------------------------------------------
-	/*!
 	@brief	デストラクタ
 	*/
 	//--------------------------------------------------------------------------------------
@@ -940,25 +975,6 @@ class creview extends crecord {
 			"1",			//条件
 			"transaction_id asc",	//並び替え
 			"limit " . $from . "," . $limit		//抽出開始行と抽出数
-		);
-		//順次取り出す
-		while($row = $this->fetch_assoc()){
-			$arr[] = $row;
-		}
-		//取得した配列を返す
-		return $arr;
-	}
-	
-	public function get_allH($debug){
-		$arr = array();
-		//親クラスのselect()メンバ関数を呼ぶ
-		$this->select(
-			$debug,			//デバッグ表示するかどうか
-			"*",			//取得するカラム
-			"review",	//取得するテーブル
-			"1",			//条件
-			"transaction_id asc"	//並び替え
-			
 		);
 		//順次取り出す
 		while($row = $this->fetch_assoc()){
