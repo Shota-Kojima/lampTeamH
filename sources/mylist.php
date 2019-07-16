6924b1a1a88055aec4a7b490003acac904df4645
ナナシーナ <vitavita3363@yahoo.co.jp>
2019-07-16 17:15:59 +0900

    update

--- sources/mylist.php
+++ sources/mylist.php
@@ -8,56 +8,45 @@ require_once($CMS_COMMON_INCLUDE_DIR . "auth_user.php");
 //ユーザIDがあるときのみ動作
 //------------------------
 if(isset($_SESSION['HTeam_adm']['customer_id']) && $_SESSION['HTeam_adm']['customer_id'] !== ""){
-    $transaction_info_obj = new ();
-    $transaction_details_obj = new ctransaction_details();
+    $favorite_obj = new cfavorite();
     //--------------------------
-    //取引情報テーブル内の全レコード取得
+    //いいねテーブル内の全レコード取得
     //--------------------------
-    $transInfoarr = $transaction_info_obj->get_allH(false,$_SESSION['HTeam_adm']['customer_id']);
-    if($transInfoarr !== false){
-        
-        //重複して入る配列
-        $cart_product=array();
-        $history= array();
-        //--------------------------
-        //１取引情報に紐づく複数の取引明細を取得し、
-        //tmplに渡す変数に格納
-        //--------------------------
-        for($i = 0; $i < count($transInfoarr); $i++){
-            $bday = new DateTime($transInfoarr[$i]['purchase_date']);
-            //明細を取得
-            $detailsarr = $transaction_details_obj->get_allH(false,$transInfoarr[$i]['transaction_id']);
-            for($j = 0; $j < count($detailsarr); $j++){
+    $favoritearr = $favorite_obj->get_allH(false,$_SESSION['HTeam_adm']['customer_id']);
+    if($favoritearr !== false){
+       
+        $view = array();
+        if(count($favoritearr)===0){
+            
+        }else{
+            for($i = 0; $i < count($favoritearr); $i++){
+
                 //商品テーブルから商品の画像とテキストなどを取得したいため持ってくる
-                $productarr = $product_obj->get_tgt(false,$detailsarr[$j]['product_id']);
+                $productarr = $product_obj->get_tgt(false,$favoritearr[$i]['product_id']);
                 
                 //取得出来たら商品の金額をセッションに格納
                 if($productarr !== false){
                     
                     $path = explode(",",$productarr['product_pass']);
-                    $history[$i][$j] = array(
-                                            'product_id'=> (int)$detailsarr[$j]['product_id'],
-                                            'transaction_id'=>(int)$detailsarr[$j]['transaction_id'],
-                                            'product_name' => (String)$productarr['product_name'],
-                                            'product_category'=> (int)$detailsarr[$j]['transaction_category'],
-                                            'product_pass'=> (String)$path[0],
-                                            'purchase_price'=> (int)$detailsarr[$j]['purchase_price'],
-                                            'purchase_value'=> (int)$detailsarr[$j]['purchase_value'],
-                                            'product_text'=>$productarr['product_text'],
-                                            'total_value'=>$transInfoarr[$i]['total_value'],
-                                            'total_money'=>$transInfoarr[$i]['total_money'],
-                                            'purchase_date'=>$bday->format('Y年m月d日')
-                                        );
+                    $view[$i] = array(
+                                    'customer_id'=> (String)$favoritearr[$i]['customer_id'],
+                                    'product_id'=>(int)$favoritearr[$i]['product_id'],
+                                    'memo' => (String)$favoritearr[$i]['memo'],
+                                    'product_name'=> (String)$productarr['product_name'],
+                                    'product_pass'=>(String)$path[0],
+                                    'price'=>(int)$productarr['price'],
+                                    'stock_value'=>(int)$productarr['stock_value']
+                                );
                 }else{
                     // echo '<script type="text/javascript">alert("64のelse");</script>';
                     //  ステータスコードを出力
                     
                 }
-            }
-            
-        } 
+            } 
+        }
         
-        $smarty->assign('history',$history);
+        
+        $smarty->assign('view',$view);
 
     }
 }
